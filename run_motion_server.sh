2be0c2ecdad931ac0372a4aae9f22cb11b759ff9
AIResearcherHZ <xhz2082416211@gmail.com>
2026-01-21 18:30:36 +0800

    将Taks_T1所有配置文件的测试动作从demo改回ceshi并更新仿真测试动作为混元数据:在taks_t1_mimic_future_config.py/taks_t1_mimic_distill_config.py及两个备份配置文件中将motion_file从taks_t1_demo.yaml改回taks_t1_ceshi.yaml,在run_motion_server.sh中将测试动作从0807_yanjie_walk_001.pkl改为20260121_104515801_e3f7cb4e_000_taks_t1.pkl混元数据集动作并保留原walk动作作为注释

--- run_motion_server.sh
+++ run_motion_server.sh
@@ -12,7 +12,8 @@ robot_type="${1:-unitree_g1_with_hands}"
 # Select motion file based on robot type
 if [ "$robot_type" == "taks_t1" ]; then
     # Taks_T1 uses 32 DOF motion files
-    motion_file="../../GMR/data/TWIST2_dataset/example_motions_taks_t1/0807_yanjie_walk_001.pkl"
+    motion_file="../../GMR/data/HUNYUAN/20260121_104515801_e3f7cb4e_000_taks_t1.pkl"
+    # motion_file="../../GMR/data/TWIST2_dataset/example_motions_taks_t1/0807_yanjie_walk_001.pkl" # 测试
     # motion_file="../../GMR/data/TWIST2_dataset/example_motions_taks_t1/accad_A5__Pick_up_box.pkl" # 难
     # motion_file="../../GMR/data/TWIST2_dataset/example_motions_taks_t1/accad_B22____side_step_left.pkl" # 抬脚权重太低
     # motion_file="../../GMR/data/TWIST2_dataset/example_motions_taks_t1/accad_D15___switch_stance.pkl"
